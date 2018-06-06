0ea4db2fdb73562b9ece59871053f3ea9511ab29
Steve <Steven.Joles@gmail.com>
2018-06-06 18:36:47 -0400

    instructor file

--- pl_downloader/downloader.py
+++ pl_downloader/downloader.py
@@ -1,11 +1,11 @@
 import sys
-from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import urllib.request
 
 
 class Downloader(QDialog):
+
     def __init__(self):
         QDialog.__init__(self)
 
@@ -14,13 +14,12 @@ class Downloader(QDialog):
         self.url = QLineEdit()
         self.save_location = QLineEdit()
         self.progress = QProgressBar()
-        download = QPushButton("download")
+        download = QPushButton("Download")
         browse = QPushButton("Browse")
         close_button = QPushButton("Cancel")
-        close_button.clicked.connect(self.close)
 
         self.url.setPlaceholderText("URL")
-        self.save_location.setPlaceholderText("file Save Location")
+        self.save_location.setPlaceholderText("File save location")
 
         self.progress.setValue(0)
         self.progress.setAlignment(Qt.AlignHCenter)
@@ -35,14 +34,16 @@ class Downloader(QDialog):
         self.setLayout(layout)
 
         self.setWindowTitle("PyDownloader")
+        self.setFocus()
 
         download.clicked.connect(self.download)
         browse.clicked.connect(self.browse_file)
+        close_button.clicked.connect(self.close)
 
     def browse_file(self):
-        save_file = QFileDialog.getSaveFileName(self, caption="Save file As:", directory=".", filter='All Files (*.*)')
-#        self.save_location.setText(QDir.toNativeSeparators(save_file))
-        self.save_location.setText(save_file)
+        save_file = QFileDialog.getSaveFileName(self, caption="Save File As", directory=".",
+                                                filter='All Files (*.*)')
+        self.save_location.setText(QDir.toNativeSeparators(save_file))
 
     def download(self):
         url = self.url.text()
@@ -51,26 +52,33 @@ class Downloader(QDialog):
         try:
             urllib.request.urlretrieve(url, save_location, self.report)
         except Exception:
-            QMessageBox.warning(self, "Warning", "the download failed")
+            QMessageBox.warning(self, "Warning", "The download failed")
             return
 
-        QMessageBox.information(self, "information", "the download is complete")
+        QMessageBox.information(self, "information", "The download is complete")
         self.progress.setValue(0)
         self.url.setText("")
         self.save_location.setText("")
-
 
     def report(self, blocknum, blocksize, totalsize):
         readsofar = blocknum * blocksize
         if totalsize > 0:
             percent = readsofar * 100 / totalsize
-            self.progress.setvalue(int(percent))
-
+            self.progress.setValue(int(percent))
 
 
 app = QApplication(sys.argv)
-dialog = Downloader()
-dialog.show()
+dl = Downloader()
+dl.show()
 app.exec_()
 
+'''
+http://ipv4.download.thinkbroadband.com/10MB.zip
 
+save_file = QFileDialog.QFileDialog(self, caption="Save file As:", directory=".", filter='All Files (*.*)')
+self.save_location.setText(save_file)
+
+close_button = QPushButton("Cancel")
+close_button.clicked.connect(self.close)
+layout.addWidget(close_button)
+'''
